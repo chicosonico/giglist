39fc7da6534128c021c5efeb6ce9c63041b5a1ed
“chicosonico” <schvarzman@gmail.com  git config --global user.name chicosonico git config --global user.email schvarzman@gmail.com>
2021-06-21 20:42:43 +0200

    added factura indirecta

--- gigs/app/controllers/home_controller.rb
+++ gigs/app/controllers/home_controller.rb
@@ -21,11 +21,19 @@ class HomeController < ApplicationController
 
     @gigs_b = current_user.gigs.where('gig_type' => "B").count
 
+    @gigs_factura_indirecta = current_user.gigs.where('gig_type' => "Factura Indirecta").count
+
+
+
     @gigs_total_amount_factura = current_user.gigs.where('gig_type' => "Factura").sum(:gig_cache)
     
     @gigs_total_amount_b = current_user.gigs.where('gig_type' => "B").sum(:gig_cache)
 
     @gigs_total_amount_altass = current_user.gigs.where('gig_type' => "Alta").sum(:gig_cache)
+
+    @gigs_total_amount_factura_indirecta = current_user.gigs.where('gig_type' => "Factura Indirecta").sum(:gig_cache)
+
+    @gigs_total_amount_altas_all = @gigs_total_amount_altass + @gigs_total_amount_factura_indirecta
 
 
     else
